3fc54c127522c97e3412d089a9c68cef646b3502
Homagni Bhattacharjee <homagni87@gmail.com>
2017-09-28 00:24:29 +0530

    Update poi_id.py

--- Project_6_Enron_Email_fraud/poi_id.py
+++ Project_6_Enron_Email_fraud/poi_id.py
@@ -22,28 +22,41 @@
 # 
 # ### **Exploring the dataset**
 
-# In[1]:
+# In[66]:
 
 
+#Importing modules
 import os
+import pickle
+import sys
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+from feature_format import featureFormat, targetFeatureSplit
+from pprint import pprint
+from tester import dump_classifier_and_data
+
+
+# In[25]:
+
+
 os.chdir('C:/ud120-projects-master/final_project')
 
 
-# In[2]:
+# In[26]:
 
 
-import pickle
 enron_data = pickle.load(open("final_project_dataset.pkl", "r"))
 
 
-# In[3]:
+# In[27]:
 
 
 # number of people
 print "There are ", len(enron_data), "excecutives in Enron Dataset"
 
 
-# In[4]:
+# In[28]:
 
 
 # name of all the excecutives
@@ -52,27 +65,27 @@ print enron_data.keys()
 
 # There are one name that was not a person name 'THE TRAVEL AGENCY IN THE PARK'. This was an outlier.
 
-# In[5]:
+# In[29]:
 
 
 enron_data.pop('THE TRAVEL AGENCY IN THE PARK', 0)
 
 
-# In[6]:
+# In[30]:
 
 
 # number of features
 print "There are ", len(enron_data['SKILLING JEFFREY K'].keys()), "features in the Enron Dataset"
 
 
-# In[7]:
+# In[31]:
 
 
 # list of features
 print "List of features: " + "\n" + str(enron_data['SKILLING JEFFREY K'].keys())
 
 
-# In[8]:
+# In[32]:
 
 
 # count people of interest
@@ -88,7 +101,7 @@ print poi_name
 
 # Let's explore something more about those POIs.
 
-# In[9]:
+# In[33]:
 
 
 #How many data have “NaN” for their "long-term-incentives"
@@ -99,7 +112,7 @@ for entry in enron_data:
 print 'NaN data for long_term_incentives:', count_total_incentives_NaN
 
 
-# In[10]:
+# In[34]:
 
 
 #How many folks in this dataset have a 'NaN' salary? 
@@ -110,7 +123,7 @@ for entry in enron_data:
 print 'NaN for salary:', count_salary
 
 
-# In[11]:
+# In[35]:
 
 
 # Any data that all 'NaN' in main features:
@@ -119,7 +132,7 @@ for entry in enron_data:
         print entry
 
 
-# In[12]:
+# In[36]:
 
 
 enron_data['LOCKHART EUGENE E']
@@ -129,19 +142,12 @@ enron_data['LOCKHART EUGENE E']
 # 
 # Let's explore some bivariative relationship between some quantitative features from the Enron dataset.
 
-# In[13]:
+# In[37]:
 
 
 # salary vs. bonus
-import sys
 
-get_ipython().magic(u'matplotlib inline')
-import matplotlib
-import numpy as np
-import matplotlib.pyplot as plt
 sys.path.append("../tools/")
-from feature_format import featureFormat, targetFeatureSplit
-
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
@@ -161,7 +167,7 @@ matplotlib.pyplot.show()
 
 # We can see that there was an outlier that was extremely higher than the rest of the data. Let's come back to the dataset and see what was the data point.
 
-# In[14]:
+# In[38]:
 
 
 # remove NaN's and find max salary 
@@ -173,7 +179,7 @@ print "Total people with salary: " + str(len(salary_list))
 print "Maxium salary was " + str(max(salary_list))
 
 
-# In[15]:
+# In[39]:
 
 
 # find person with max salary
@@ -184,22 +190,21 @@ for entry in enron_data:
 
 # So, the outlier was the TOTAL salary and bonus. I removed it from the dataset
 
-# In[16]:
+# In[40]:
 
 
 # remove outlier from the data
 enron_data.pop('TOTAL',0)
 
 
-# In[17]:
+# In[41]:
 
 
-import numpy as np
 # mean salary
 print "Average salary: " + str(int(np.mean(salary_list)))
 
 
-# In[18]:
+# In[42]:
 
 
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
@@ -221,11 +226,10 @@ matplotlib.pyplot.show()
 
 # We would argue that there’s 4 more outliers to investigate; let's look at a couple of them. Two people made bonuses of at least 5 million dollars, and a salary of over 1 million dollars; in other words, they got richly rewarded. What are the names associated with those point? Are they POIs?
 
-# In[19]:
+# In[43]:
 
 
 # catch outliers with high salary
-from pprint import pprint
 outliers_salary = []
 for key in enron_data:
     val = enron_data[key]['salary']
@@ -238,7 +242,7 @@ top_4 = sorted(outliers_salary,key=lambda x:x[1],reverse=True)[:4]
 pprint(top_4)
 
 
-# In[20]:
+# In[44]:
 
 
 # are they POIs?
@@ -252,7 +256,7 @@ for people in top_4:
 # ### 4. Features processing
 # The data has been cleaned. Next step I added a new feature. From the intuition about the data, I guessed that the more message exchanged among POIs (fraction of from_this_person_to_poi and from_poi_to_this_person to total_message) the more chance of the person is POI.
 
-# In[21]:
+# In[45]:
 
 
 def get_total_list(key1, key2):
@@ -267,21 +271,21 @@ def get_total_list(key1, key2):
     return new_list
 
 
-# In[22]:
+# In[46]:
 
 
 # get the total poi related emails:
 total_poi_emails = get_total_list('from_this_person_to_poi', 'from_poi_to_this_person')
 
 
-# In[23]:
+# In[47]:
 
 
 # get the total emails
 total_emails = get_total_list('to_messages', 'from_messages')
 
 
-# In[24]:
+# In[48]:
 
 
 def fraction_list(list1, list2):
@@ -298,14 +302,14 @@ def fraction_list(list1, list2):
     return fraction
 
 
-# In[25]:
+# In[49]:
 
 
 # get the fraction of poi emails
 fraction_poi_emails = fraction_list(total_poi_emails, total_emails)
 
 
-# In[26]:
+# In[50]:
 
 
 # add this new feature to my data
@@ -318,14 +322,14 @@ for i in enron_data:
 print 'SKILLING fraction_poi_emails: ', enron_data['SKILLING JEFFREY K']['fraction_poi_emails']
 
 
-# In[27]:
+# In[51]:
 
 
 # let's test if this feature has any correlation with POIs
 new_features_list = ['poi', 'fraction_poi_emails']
 data = featureFormat(enron_data, new_features_list)
 
-get_ipython().magic(u'matplotlib inline')
+
 ### your code below
 for point in data:
     poi = point[0]
@@ -345,20 +349,19 @@ matplotlib.pyplot.show()
 # 
 # #### Original features
 
-# In[28]:
+# In[52]:
 
 
-import pickle
-import sys
-sys.path.append("../tools/")
-from feature_format import featureFormat, targetFeatureSplit
 from sklearn.cross_validation import train_test_split
-import numpy as np
+
 np.random.seed(42)
+
 from time import time
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
 
 # load the data
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
@@ -373,13 +376,11 @@ labels, features = targetFeatureSplit(data)
 features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.4, random_state = 42)
 
 # choose decision tree
-from sklearn.tree import DecisionTreeClassifier
 t0 = time()
 clf = DecisionTreeClassifier()
 clf = clf.fit(features_train, labels_train)
 pred = clf.predict(features_test)
 
-from sklearn.metrics import accuracy_score
 acc = accuracy_score(labels_test, pred)
 print 'Accuracy: ' + str(acc)
 print 'Precision: ', precision_score(labels_test, pred)
@@ -397,7 +398,7 @@ for i in range(19):
 
 # #### Add new features
 
-# In[29]:
+# In[53]:
 
 
 # features_list
@@ -410,13 +411,11 @@ labels, features = targetFeatureSplit(data)
 features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.4, random_state = 42)
 
 # choose decision tree
-from sklearn.tree import DecisionTreeClassifier
 t0 = time()
 clf = DecisionTreeClassifier()
 clf = clf.fit(features_train, labels_train)
 pred = clf.predict(features_test)
 
-from sklearn.metrics import accuracy_score
 acc = accuracy_score(labels_test, pred)
 print 'Accuracy: ' + str(acc)
 print 'Precision: ', precision_score(labels_test, pred)
@@ -442,17 +441,26 @@ for i in range(20):
 # 
 # The chosen method was scikit learn’s SelectKBest using f_classif as scoring function. The f_classif function computes the ANOVA F-value between labels and features for classification tasks.
 # 
-# A total of 10 features was used as using too many features may cloud the result. By taking 10, we will have a balance between using the best features as well as capture the overall data picture
+# Now we have seen better accuracy value when we used the original feature set which did not incude the new feature created. In the previous case, a total of nine features had any score above 0, hence those features are chosen for next round which are are as follows:
+# 
+# Accuracy: 0.844827586207
+# Precision:  0.333333333333
+# Recall:  0.125
+# Decision Tree algorithm run time:  0.06 s
+# Feature Ranking: 
+# 1 feature salary (0.337437907714)
+# 2 feature from_poi_to_this_person (0.161410018553)
+# 3 feature from_this_person_to_poi (0.109563164109)
+# 4 feature to_messages (0.101688311688)
+# 5 feature deferral_payments (0.0753246753247)
+# 6 feature total_payments (0.0753246753247)
+# 7 feature exercised_stock_options (0.0564935064935)
+# 8 feature bonus (0.048961038961)
+# 9 feature restricted_stock (0.0337967018319)
 
-# In[30]:
+# In[55]:
 
 
-import pickle
-import sys
-sys.path.append("../tools/")
-from feature_format import featureFormat, targetFeatureSplit
-from sklearn.cross_validation import train_test_split
-import numpy as np
 np.random.seed(42)
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.grid_search import GridSearchCV
@@ -462,7 +470,7 @@ from sklearn.pipeline import Pipeline
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
 # features_list
-features_list = ['salary', 'to_messages', 'deferral_payments', 'total_payments', 'exercised_stock_options', 'bonus', 'restricted_stock', 'shared_receipt_with_poi', 'restricted_stock_deferred', 'total_stock_value', 'expenses', 'loan_advances', 'from_messages', 'other', 'from_this_person_to_poi', 'poi', 'director_fees', 'deferred_income', 'long_term_incentive', 'from_poi_to_this_person']
+features_list = ['salary', 'from_poi_to_this_person', 'exercised_stock_options', 'bonus', 'restricted_stock',  'total_payments', 'from_this_person_to_poi', 'to_messages', 'deferral_payments']
 
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
@@ -470,7 +478,7 @@ labels, features = targetFeatureSplit(data)
 # split data into training and testing
 features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.4, random_state = 42)
 from sklearn.feature_selection import SelectKBest
-selector = SelectKBest(k=10)
+selector = SelectKBest(k='all')
 selectedFeatures = selector.fit(features,labels)
 feature_names = [features_list[i] for i in selectedFeatures.get_support(indices=True)]
 print 'Best features: ', feature_names
@@ -479,15 +487,10 @@ print 'Best features: ', feature_names
 # We can see most of the features here are financial features which is consistent with the nature of the of the task at hand.
 # Let's use the features and run the again the Decision Tree to see any improvement
 
-# In[31]:
+# In[56]:
 
 
-import pickle
-import sys
-sys.path.append("../tools/")
-from feature_format import featureFormat, targetFeatureSplit
-from sklearn.cross_validation import train_test_split
-import numpy as np
+
 np.random.seed(42)
 from time import time
 from sklearn.metrics import precision_score
@@ -527,12 +530,12 @@ print 'Decision Tree algorithm run time: ', round(time()-t0, 3), 's'
 # Since Decision Tree algorithm was used which does not need scaling, feature scaling was not used.
 # 
 # 
-# ### Algorithm Selection and Tunning
+# ### Algorithm Selection and Tuning
 # I have tried Decision tree. With the small samples like that Naive Bayes may not work very well. So, let's try Random Forest, Adaboost and NearestK.
 # 
 # #### Random Forest
 
-# In[32]:
+# In[57]:
 
 
 features_list = ['poi','deferral_payments', 'exercised_stock_options', 'bonus', 'restricted_stock_deferred', 'total_stock_value', 'expenses', 'from_messages', 'other', 'director_fees', 'deferred_income']
@@ -560,7 +563,7 @@ print 'Random algorithm run time: ', round(time()-t0, 3), 's'
 # 
 # #### Adaboost
 
-# In[33]:
+# In[58]:
 
 
 from sklearn.ensemble import AdaBoostClassifier
@@ -581,7 +584,7 @@ print 'Adaboost algorithm run time: ', round(time()-t0, 3), 's'
 # 
 # #### Logistic Regression
 
-# In[34]:
+# In[59]:
 
 
 from sklearn.linear_model import LogisticRegression
@@ -603,7 +606,7 @@ print 'Logistic regression algorithm run time: ', round(time()-t0, 3), 's'
 # 
 # #### Nearest K
 
-# In[35]:
+# In[60]:
 
 
 from sklearn.neighbors.nearest_centroid import NearestCentroid
@@ -624,7 +627,7 @@ print 'Nearest K algorithm run time: ', round(time()-t0, 3), 's'
 # 
 # #### SVC
 
-# In[36]:
+# In[61]:
 
 
 from sklearn import svm
@@ -655,7 +658,7 @@ print 'SVC algorithm run time: ', round(time()-t0, 3), 's'
 
 # #### Decision Tree
 
-# In[78]:
+# In[64]:
 
 
 param_grid = {'max_depth': [1, 2, 3, 4, 5],
@@ -694,7 +697,7 @@ print 'Decision Tree algorithm run time: ', round(time()-t0, 3), 's'
 # 
 # The Classifier performance, here Decision Tree classifier waqs validated by fitting the best estimator parameter set with test data which is really a subset of the training data held back from the machine learning algorithms until the very end. After we have selected and tuned the machine learning algorithms on your training dataset we can evaluate the learned models on the validation dataset to get a final objective idea of how the models might perform on unseen data, which makes validation extremely important.
 # 
-# This validation was proceeded with the following steps. 
+# This validation was proceeded with the following steps uing the cross validation vodule of sklearn.
 # First I used accuracy to evaluate my algorithm. It was a mistake because in this case we have a class imbalance problem - the number of POIs is small compared to the total number of examples in the dataset. 
 # So I had to use precision and recall for these activities instead. I was able to reach average value of precision and recall was much better  of 0.67 and 0.5 respectively.
 # 
@@ -702,23 +705,25 @@ print 'Decision Tree algorithm run time: ', round(time()-t0, 3), 's'
 # 
 # In simple terms, high precision means that an algorithm returned substantially more relevant results than irrelevant ones, while high recall means that an algorithm returned most of the relevant results.
 # 
-# Wrt to this project, 0.67 precision means that the POI identified was correct 67% times while 0.5 recall value means the 50% of the POI returned are relevant.
+# Wrt to this project, 0.3 precision means that the POI identified was correct 30% times while 0.375 recall value means the 37.5% of the POI returned are relevant.
 
 # #### Saving Data
 # 
 # So finally,I would save the classifier, dataset and feature list as three pickle files (my_dataset.pkl, my_classifier.pkl, my_feature_list.pkl) respectively.
 
-# In[79]:
+# In[67]:
 
-
-from sklearn.externals import joblib
-joblib.dump(clf, 'my_classifier.pkl')
 
 ### Store to my_dataset for easy export below.
-dataset = data_dict
-joblib.dump(dataset, 'my_dataset.pkl')
+my_dataset = data_dict
 
-joblib.dump(features, 'my_feature_list.pkl')
+dump_classifier_and_data(clf, my_dataset, features_list)
+
+
+# In[ ]:
+
+
+
 
 
 # #### Resources
